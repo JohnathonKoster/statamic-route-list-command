b7ba0bfbb2718e9b009ce8397e0179cc3694908d
John Koster <john@stillat.com>
2016-12-18 11:24:12 -0600

    You can now view addon routes

--- RouteList/Commands/RouteListCommand.php
+++ RouteList/Commands/RouteListCommand.php
@@ -3,13 +3,17 @@
 namespace Statamic\Addons\RouteList\Commands;
 
 use Illuminate\Routing\Router;
+use Statamic\CP\Router as StatamicRouter;
 use Illuminate\Foundation\Console\RouteListCommand as IlluminateRouteListCommand;
 
 class RouteListCommand extends IlluminateRouteListCommand
 {
-    
     public function __construct()
     {
+        /** @var StatamicRouter $statamicRouter */
+        $statamicRouter = app(StatamicRouter::class);
+        $statamicRouter->bindAddonRoutes();
+
         parent::__construct(app(Router::class));
     }
 
